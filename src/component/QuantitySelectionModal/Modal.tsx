033f51506ff510118a6caf980711d9dceaf0fe94
louis-stintzy <louis.stintzy@gmail.com>
2024-12-09 11:46:26 +0000

    feat: close modal when clicking on the backdrop

--- src/component/QuantitySelectionModal/Modal.tsx
+++ src/component/QuantitySelectionModal/Modal.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useRef } from 'react';
 import { useQuantitySelectionModalisOpen } from '../../store/selectors';
+import handleModalClosing from '../../utils/handleModalClosing';
 
 interface ModalProps {
   children: React.ReactNode;
@@ -10,6 +11,10 @@ function Modal({ children, className }: ModalProps) {
   const modalRef = useRef<HTMLDialogElement>(null);
   const isOpen = useQuantitySelectionModalisOpen();
 
+  const handleModalClick = (e: React.MouseEvent) => {
+    if (e.target === modalRef.current) handleModalClosing();
+  };
+
   useEffect(() => {
     if (isOpen) {
       modalRef.current?.showModal();
@@ -19,7 +24,12 @@ function Modal({ children, className }: ModalProps) {
   }, [isOpen]);
 
   return (
-    <dialog id="modal" ref={modalRef} className={className}>
+    <dialog
+      id="modal"
+      ref={modalRef}
+      className={className}
+      onClick={(e) => handleModalClick(e)}
+    >
       {children}
     </dialog>
   );
