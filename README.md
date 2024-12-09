ce197be70bed87878bdad70883e87b89f0794353
louis-stintzy <louis.stintzy@gmail.com>
2024-12-09 11:11:37 +0000

    fix: update image paths for production compatibility

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 The aim is to reproduce [this page](https://cda-tp-react-01.vercel.app/) using react.
 
 ![Les meilleures pizzas artisanales](/src/assets/capture.png)
+
+## My app on Vercel
+
+You can check out my deployed app on Vercel by clicking [here](pizzeria-sepia.vercel.app).
--- src/constants/pizzas.ts
+++ src/constants/pizzas.ts
@@ -1,31 +1,36 @@
+import margherita from '../assets/margherita.jpg';
+import regina from '../assets/regina.jpg';
+import calzone from '../assets/calzone.jpg';
+import vegetarienne from '../assets/vegetarienne.jpg';
+
 import { Pizza } from '../@types/pizza';
 
 const pizzas: Pizza[] = [
   {
     id: 1,
     name: 'Margherita',
-    picture: '/src/assets/margherita.jpg',
+    picture: margherita,
     toppings: 'Tomate, mozzarella, basilic frais',
     price: 12.9,
   },
   {
     id: 2,
     name: 'Regina',
-    picture: '/src/assets/regina.jpg',
+    picture: regina,
     toppings: 'Tomate, mozzarella, jambon, champignons',
     price: 14.9,
   },
   {
     id: 3,
     name: 'Calzone',
-    picture: '/src/assets/calzone.jpg',
+    picture: calzone,
     toppings: 'Tomate, mozzarella, jambon, œuf',
     price: 15.9,
   },
   {
     id: 4,
     name: 'Végétarienne',
-    picture: '/src/assets/vegetarienne.jpg',
+    picture: vegetarienne,
     toppings: 'Tomate, légumes grillés, olives',
     price: 13.9,
   },
